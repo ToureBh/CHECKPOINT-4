b40da1708c51a01ba88d38cd98cbc0d689246a5d
ToureBh <149252642+ToureBh@users.noreply.github.com>
2024-04-02 15:22:15 +0200

    Add files via upload

---
+++ Tarea.py
@@ -0,0 +1,79 @@
+#EXERCISE 1
+
+#List
+my_list = [1, 2, 3, 4]
+
+#Tuple
+my_tuple = ("this", "is", "my tuple")
+
+#Float
+my_float = 40.6
+
+#Integer
+my_integer = 10
+
+#Decimal
+from decimal import Decimal 
+
+my_decimal = Decimal(23.22648756494)
+
+#Dictionary
+my_dictionary = {
+    "saludo" : "hola"
+    "despedida" : "adiós"
+}
+
+#EXERCISE 2
+
+import math
+
+a_float = 40.5 
+round = math.ceil(a_float)
+
+#EXERCISE 3
+
+import math
+
+this_float = 30.3
+square_root = math.sqrt(this_float)
+
+#EXERCISE 4 
+
+a_dictionary = {
+  "first": "buy groceries",
+  "second": "cook meals",
+  "third": "clean the floor"
+}
+
+first_elmnt = a_dictionary["first"]
+
+#EXERCISE 5
+
+a_tuple = ("holaa", "este", "es", "mi", "tuple")
+
+second_elmnt = a_tuple[1]
+
+#EXERCISE 6
+
+a_list = [1, 2, 3, 4, 5]
+
+a_list.append(6)
+
+#EXERCISE  7
+
+a_list = [1, 2, 3, 4, 5]
+a_list[0] = 100
+
+#EXERCISE 8
+
+new_list = ["María", "Alba", "Adrián", "Jose", "Marco"]
+
+new_list.sort()
+
+#EXERCISE 9
+
+a_tuple = ("mi", "nuevo", "tuple")
+
+a_tuple = a_tuple + ("es este")
+
+
